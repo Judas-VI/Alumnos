5bf5d6f2f41dad571ab9722e04c3ac0e1de2f02d
Agustin Noe <agustin.urena6886@alumnos.udg.mx>
2025-10-21 02:38:56 -0600

    correciones para mostrar el error espesifico

--- resources/views/alumnos/edit-alumno.blade.php
+++ resources/views/alumnos/edit-alumno.blade.php
@@ -15,29 +15,48 @@
     <form action="{{ route('alumno.update', $alumno->id) }}" method="POST">
         @csrf
         @method('PATCH')
+        
         <label for="codigo">Código:</label>
         <input type="text" id="codigo" name="codigo" value="{{ old('codigo') }}">
+        @error('codigo')
+        <span style="color: red; font-size: 0.9em;">{{ $message }}</span>
+        @enderror
         <br>
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" name="nombre" value="{{ old('nombre') }}">
+        @error('nombre')
+        <span style="color: red; font-size: 0.9em;">{{ $message }}</span>
+        @enderror
         <br>
         <label for="correo">Correo:</label>
         <input type="email" id="correo" name="correo" value="{{ old('correo') }}">
+        @error('correo')
+        <span style="color: red; font-size: 0.9em;">{{ $message }}</span>
+        @enderror
         <br>
         <label for="fecha_nacimiento">Fecha Nacimiento:</label>
         <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" value="{{ old('fecha_nacimiento') }}">
+        @error('fecha_nacimiento')
+        <span style="color: red; font-size: 0.9em;">{{ $message }}</span>
+        @enderror
         <br>
         <fieldset>
             <legend>Sexo:</legend>
             <label>
-                <input type="radio" name="sexo" value="M" {{ old('sexo') == 'M' ? 'checked' : '' }}> M
+                <input type="radio" name="sexo" value="M"> M
             </label>
             <label>
-                <input type="radio" name="sexo" value="F" {{ old('sexo') == 'F' ? 'checked' : '' }}> F
+                <input type="radio" name="sexo" value="F"> F
             </label>
+            @error('sexo')
+            <span style="color: red; font-size: 0.9em; display: block;">{{ $message }}</span>
+            @enderror
         </fieldset>
         <label for="carrera">Carrera:</label>
         <input type="text" id="carrera" name="carrera" value="{{ old('carrera') }}">
+        @error('carrera')
+        <span style="color: red; font-size: 0.9em;">{{ $message }}</span>
+        @enderror
         <br>
         <button type="submit">Guardar</button>
     </form>
